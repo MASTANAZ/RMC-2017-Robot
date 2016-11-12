3324941dadedbef678b47e67e07a508f01d242fc
Blake Nazario <BlakeNazario@Blakes-iMac.local>
2016-11-12 00:15:18 -0500

    Added manual controls for controlling true state. Laid groundwork for animating changes each state.

--- Algorithm-Sandbox/GridWorld.m
+++ Algorithm-Sandbox/GridWorld.m
@@ -1,19 +1,26 @@
 % GridWorld.m
 clear all;
+clc;
+clf;
+
 
 % Create the set of actions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 create_actions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Display new restriction message to user
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp("As of 11/11/2018, the input received only corresponds to what angle the robot is to move to.")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Some simulaton parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 gridDims = [12 4 16 4 4 2]; % number of rows, number of columns in the three regions, number of rows and columns of unload station
-pObstacle = 0.1; % per-cell probability of obstacle being present (in region 2)
-pStartTop = 0.5; % probability of rover starting in top-left (vs bottom-left) corner
+pObstacle = 0.1;  % per-cell probability of obstacle being present (in region 2)
+pStartTop = 0.5;  % probability of rover starting in top-left (vs bottom-left) corner
 pTransSpd = 0.01; % rover's translational speed (assuming flat terrain, in units of world width per decision stage)
-pAngleSpd = 5.0; % rover's angular speed (in units of degrees per decision stage)
+pAngleSpd = 5.0;  % rover's angular speed (in units of degrees per decision stage)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Initialze true state of 3-region grid
@@ -40,14 +47,123 @@ clear region1 region2 region3 vPos;
 
 k = 0;
 while(1) % Enter control loop
+
   visualizeGrid(trueCells,trueRover,k,estiCells,estiRover);
-  disp(['Decision stage k = ' num2str(k) ': This is where we can prompt user to enter a next goal/command/control, eventually assuming the true state is hidden and so it can be based only on the estimated state']);
-  result = input('--> So, what is the control space and (when eventually assuming imperfect state information) how will the (estimated) state that drives the control policy be generated? [for now, just hit enter to proceed]');
+
+  % If current theta > 360, subtract 360 iteratively to prevent
+  % reduntant theta representations
+  if trueRover.pos(3) >= 360
+    for test = trueRover.pos(3):-360:0
+      trueRover.pos(3) = test
+    endfor
+  endif
   
-  %Here, take input [1:9] and translate it to rover.pos
-  trueRover.pos = [gridDims(1)/sum(gridDims(2:4)); ... % vertical position in units of world height
-                 rand*gridDims(6)/sum(gridDims(2:4)); ... % horizontal position in units of world width
-                 rand*360];
+  % If theta is negative, make it the equivalent positive theta
+  if trueRover.pos(3) < 0
+    trueRover.pos(3) = 360 - abs(trueRover.pos(3))
+  endif
+  
+  % disp(['Decision stage k = ' num2str(k) ': This is where we can prompt user to enter a next goal/command/control, eventually assuming the true state is hidden and so it can be based only on the estimated state']);
+  
+  disp(["--> So, what is the control space and (when eventually assuming imperfect state information) how will the (estimated) state that drives the control policy be generated? [for now, just hit enter to proceed]"])
+  
+  % Receive command from user
+  result = input(["\n\nCommand at k=" num2str(k) ": "]);
+  
+  % Display rovers current real position (result = 0)
+  if result == 0    
+    printf("\n*** TRUE POSITION ***\n\n")
+    disp(trueRover.pos)
+    printf("*********************\n\n\n\n")
+  endif
+  
+  % Receive number of grid spaces to move from user
+  spacesToMove = input("Number of grid spaces to move: ");
+  
+  
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Here, take input [1:9] and translate it to rover.pos
+  
+  
+  % Command does not ask to display current real position
+  if result != 0
+    
+    % Command exists in "actions" list
+    if find(actions==result)
+      [nextX, nextY, nextTheta, deltaTheta, deltaStep_Theta, deltaStep_Move] = user_update_position(result, spacesToMove, trueRover, actions, pTransSpd, pAngleSpd)
+
+      % Cycle through the steps here.
+      % First, rotate the rover
+      for iStep = k+1:1:ceil(deltaStep_Theta + k)
+        
+        % Correctly represent rotation to new theta
+        if (sqrt(((trueRover.pos(3) - nextTheta))^2)) <= 180
+          
+          % No change in theta
+          if deltaTheta == 0
+            disp("\n*** No change in theta\n")
+            trueRover.pos(3) += 0;
+          
+          % Iteratively add to current theta
+          % Negative delta-theta = clockwise rotation
+          % Positive delta-theta = counter-clockwise rotation
+          else 
+            %disp("\n*** delta < 180 : Rotate clockwise, Theta_1 >= Theta_2\n")
+            trueRover.pos(3) += (((deltaTheta)/pAngleSpd) / deltaStep_Theta) * pAngleSpd;  
+          endif
+         
+        else 
+          %trueRover.pos(3) += abs((min([nextTheta, trueRover.pos(3)]) - max([nextTheta, trueRover.pos(3)]) + 360);
+          % Subtract angle to rotate clockwise          
+          if nextTheta >= trueRover.pos(3)
+            %disp("\n*** delta >180 : Rotate clockwise, Theta_1 <= Theta_2\n")
+            trueRover.pos(3) -= sqrt((min([nextTheta, trueRover.pos(3)]) - max([nextTheta, trueRover.pos(3)]) + 360)^2);;
+            
+          % Add angle to rotate counter clockwise
+          else 
+            %disp("\n*** delta > 180 : Rotate counter-clockwise, Theta_1 >= Theta_2\n")
+            trueRover.pos(3) += sqrt((min([nextTheta, trueRover.pos(3)]) - max([nextTheta, trueRover.pos(3)]) + 360)^2);
+          endif
+        endif
+        
+        k = iStep;
+        
+        % pause for animation
+        %pause(0.1)
+        % SPRITE ANIMATION FUNCTION HERE
+        % Animate current step in iteration
+        %visualizeGrid(trueCells,trueRover,k,estiCells,estiRover);
+      endfor
+      
+      for jStep = k+1:1:ceil(deltaStep_Move + k)
+        trueRover.pos(2) += (((nextX-trueRover.pos(2))/pTransSpd) / deltaStep_Move) * pTransSpd;
+        trueRover.pos(1) += (((nextY-trueRover.pos(1))/pTransSpd) / deltaStep_Move) * pTransSpd;
+        
+        k = ceil(jStep);
+        
+        % pause for animation
+        %pause(0.1)
+        % SPRITE ANIMATION FUNCTION HERE
+        % Animate current step in iteration
+        %visualizeGrid(trueCells,trueRover,k,estiCells,estiRover);
+      endfor
+      
+      % steps are integers >= 0, round k towards positive infinity.
+      k = ceil(k);
+     
+    % The command is invalid
+    else
+      disp("\n\n*** Error: Invalid input- input must be any integer 1 through 9.\n\n")
+    endif
+  endif
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  
   
   disp('--> This is where the outcome of that goal/command/control is simulated (including uncertainty due to system disturbance, sensor noise, etc.), the true state is updated and the single-stage cost is recorded');
   k = k + 1;
