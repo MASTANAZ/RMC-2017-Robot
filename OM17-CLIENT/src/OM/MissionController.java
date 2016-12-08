f018b7f14357156f52799d7bc402d6371a317d20
harris_n <newstederharris@gmail.com>
2016-12-08 09:37:18 -0500

    added a dedicated pane for monitoring robot status so that multiple robots can be observed simultaneously in the status pane in the lower right hand corner of the mission pane

--- OM17-CLIENT/src/OM/MissionController.java
+++ OM17-CLIENT/src/OM/MissionController.java
@@ -1,14 +1,11 @@
 package OM;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
-import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,12 +16,22 @@ public class MissionController implements Initializable {
     @FXML
     private Pane fieldPane;
 
+    @FXML
+    private Pane statusPane;
+
     public MissionController() {
-        System.out.println("MISSION CONTROLLER");
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         fieldPane.getChildren().add(new FieldCanvas(fieldPane));
+
+        try {
+            statusPane.getChildren().add(FXMLLoader.load(getClass().getClassLoader().getResource("res/robot_status_pane.fxml")));
+            statusPane.getChildren().add(FXMLLoader.load(getClass().getClassLoader().getResource("res/robot_status_pane.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
