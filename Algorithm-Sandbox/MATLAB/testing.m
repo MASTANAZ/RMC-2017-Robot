cf50f8c25fcfdcdd01b8e3b40493d7fead7dafbd
blake <blake.nazario@yahoo.com>
2017-01-02 21:25:55 -0500

    This closes #9. Added simple D* Algorithm to Algorithm Sandbox code. To see the example, run the Testing.m file.

---
+++ Algorithm-Sandbox/MATLAB/testing.m
@@ -0,0 +1,10 @@
+%  map = zeros(24,12)
+addpath('D-Star')    % Adding path directory for all d-star file
+
+load map1;
+goal = [6,24];
+start=[1,1];
+ds = Dstar(map);    % create navigation object
+ds.plan(goal)       % create plan for specified goal
+ds.plot(goal)
+%        ds.path(start)      % animate path from this start location
