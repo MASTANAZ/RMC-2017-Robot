fc4292a5c2f08bd3e0d453519d40ea9477c09428
Harris Newsteder <newstederharris@gmail.com>
2017-01-20 18:37:49 -0500

    sending robot position data over the network to the MC

--- om_ws/build/om17/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/unf_rmc_2017/om_ws/src/om17
-# Build directory: /home/ros/unf_rmc_2017/om_ws/build/om17
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
