c33eefa99a482755086f8ffceb6286b833a0778d
Harris Newsteder <newstederharris@gmail.com>
2017-01-24 16:13:33 -0500

    cleaned up the catkin workspace and now the ROS environment loads properly at runtime for both deimos and phobos given ~/robot_info.json exists

--- om_ws/build/om17/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phobos/unf_rmc_2017/om_ws/src/om17
-# Build directory: /home/phobos/unf_rmc_2017/om_ws/build/om17
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
