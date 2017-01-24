53dbe68fb2f0c17fde4bdb9ff1fe6ebb6449d920
Harris Newsteder <newstederharris@gmail.com>
2017-01-24 14:26:15 -0500

    first steps for ROS running across both the operation machines, added a script at startup that exports environment variables for ROS

--- om_ws/src/om17/scripts/network.py
+++ om_ws/src/om17/scripts/network.py
@@ -28,14 +28,7 @@ _S_P_ORIENTATION   = 0x03
 _MC_PORT         = 12000
 _MC_BUFFER_SIZE  = 1024
 _MC_RECV_TIMEOUT = 0.05
-_MC_IP           = None
-
-# robot communication parameters
-_RB_PORT         = 12001
-_RB_BUFFER_SIZE  = 512
-_RB_RECV_TIMEOUT = 0.05
-_RB_OTHER_IP     = None
-_RB_SELF_IP      = None
+_MC_IP           = rospy.get_param("/om/robot/mc_ip")
 
 _CONNECTION_KEY = "@"
 _CONFIRMATION_KEY = "!"
@@ -102,24 +95,10 @@ def network():
 ################################################################################
 
 def _init():
-    global _MC_IP, _RB_SELF_IP, _RB_OTHER_IP
-    try:
-        with open(rospy.get_param("/robot/info_file")) as data_file:
-            data = json.load(data_file)
-            
-            _MC_IP       = data["MC_IP"]
-            _RB_OTHER_IP = data["OTHER_IP"]
-            _RB_SELF_IP  = data["SELF_IP"]
-    except:
-        rospy.logfatal("FAILED TO READ " + rospy.get_param("/robot/info_file"))
-        
-    _mc_init()
-    _rb_init()
-            
+    _mc_init()  
 
 def _tick():
     _mc_tick()
-    _rb_tick()
 
 def _mc_init():
     rospy.loginfo("INITIALIZING MISSION CONTROL CONNECTION")
@@ -163,130 +142,6 @@ def _mc_connect():
             rospy.logfatal("FAILED TO RECEIVE CONFIRMATION KEY FROM MISSION CONTROL")
     except:
         rospy.logfatal("FAILED TO CONNECT TO MISSION CONTROL")
-    
-def _rb_init():
-    rospy.loginfo("INITIALIZING ROBOT COMMUNICATIONS")
-    rospy.loginfo("SEARCHING FOR HOST ON LAN")
-
-    tmp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tmp.settimeout(1)
-
-    host_found = False
-
-    try:
-        tmp.connect((_RB_OTHER_IP, _RB_PORT))
-        rospy.loginfo("ROBOT HOST FOUND")
-        host_found = True
-        
-        # sleep so the host can fail the tmp client for the connection handshake
-        time.sleep(1)
-    except:
-        rospy.loginfo("NO HOST FOUND FOR ROBOT COMMUNICATION")
-
-    tmp.close()
-
-    if host_found:
-        _rb_connect()
-    else:
-        _rb_host()
-
-def _rb_tick():
-    global _rb_pending
-
-    if _rb_hosting:
-        # no client has connected yet
-        if not _rb_connected:
-            try:
-                conn, addr = _rb.accept()
-                _rb_process_client(conn)
-            except:
-                pass
-        else:
-            pass
-    # this robot is the client to the other robot's server
-    else:
-        if not _rb_connected: return
-        
-        if len(_rb_pending) > 0:
-            _rb.send(_rb_pending)
-            _rb_pending = ""
-
-def _rb_connect():
-    global _rb, _rb_connected
-
-    rospy.loginfo("ATTEMPTING TO CONNECT TO ROBOT HOST AT " + _RB_OTHER_IP)
-
-    try:
-        _rb.connect((_RB_OTHER_IP, _RB_PORT))
-        
-        rospy.loginfo("SENDING CONNECTION KEY")
-        _rb.send(_CONNECTION_KEY)
-        
-        # wait for host to process connection key and send back a confirmation
-        time.sleep(0.5)
-        
-        confirmation = _rb.recv(16)
-        
-        if confirmation == _CONFIRMATION_KEY:
-            rospy.loginfo("CONFIRMATION KEY RECEIVED FROM HOST")
-            rospy.loginfo("SUCCESSFULLY CONNECTED TO ROBOT HOST")
-            
-            _rb_connected = True
-        else:
-            rospy.logfatal("FAILED TO RECEIVE CONFIRMATION KEY FROM ROBOT HOST")
-    except:
-        rospy.logfatal("FAILED TO CONNECT TO ROBOT HOST")
-    
-    # TODO: keep retrying connection
-
-def _rb_host():
-    global _rb, _rb_hosting
-
-    rospy.loginfo("ATTEMPTING TO CREATE ROBOT SERVER AT " + _RB_SELF_IP)
-
-    try:
-        _rb.bind((_RB_SELF_IP, _RB_PORT))
-        _rb.listen(1)
-        
-        rospy.loginfo("SUCCESFULLY CREATED ROBOT SERVER AT " + _RB_SELF_IP)
-        
-        _rb_hosting = True
-    except:
-        rospy.logfatal("FAILED TO CREATE ROBOT SERVER")
-
-def _rb_process_client(client):
-    global _rb_client, _rb_connected
-
-    rospy.loginfo("ATTEMPTING TO PROCESS NEW CLIENT")
-
-    # wait for the connecting client to send the connection key
-    time.sleep(0.25)
-
-    try:
-        key = client.recv(16)
-    except:
-        rospy.logwarning("NO CONNECTION KEY RECEIVED")
-        rospy.logwarning("TERMINATING CONNECTION")
-        
-        client.close()
-        
-        return
-
-    if key == _CONNECTION_KEY:
-        rospy.loginfo("CONNECTION KEY ACCEPTED")
-        rospy.loginfo("SENDING CONFIRMATION KEY TO CLIENT")
-        
-        client.send(_CONFIRMATION_KEY)
-        
-        _rb_client = client
-        _rb_connected = True
-        
-        rospy.loginfo("CLIENT SUCCESSFULLY ADDED")
-    else:
-        rospy.logwarning("UNKNOWN CLIENT")
-        rospy.logwarning("TERMINATING CONNECTION")
-        
-        client.close()
         
 ################################################################################
 # ENTRY POINT
