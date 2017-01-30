cac81220591778081bbb6ef113e695f1ab6b1413
harris_n <newstederharris@gmail.com>
2017-01-30 12:33:19 -0500

    the drivers for the arduino mega are being rewritten in C++ and put into different modules, the IR sensor code is now a c++ class so you can have multiple sensors running at the same time

--- om_drivers/om_mega/om_mega.ino
+++ om_drivers/om_mega/om_mega.ino
@@ -1,9 +1,24 @@
+// om_mega
+// 
+// CREATED BY HARRIS NEWSTEDER AND BLAKE NAZARIO-CASEY
+//
+// OPERATION CODE FOR THE ON-BOARD ARDUINO MEGA
+// NASA RMC 2017
+
+#include "ir_sense.h"
+
+#include <Servo.h>
+
+IRSense ir;
+
 void setup() {
   Serial.begin(9600);
+
+  ir.setSensorPin(8);
+  ir.setServoPin(11);
 }
 
 void loop() {
-  Serial.println("test");
-  delay(1000);
+  ir.tick();
 }
 
