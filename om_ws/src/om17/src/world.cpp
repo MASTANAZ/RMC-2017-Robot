f6bd52c96a057ecfaa6d80755f33857a7fcf6bf7
Harris Newsteder <newstederharris@gmail.com>
2017-03-05 11:19:16 -0500

    fixed function names in the world node to match ROS style and added the callback for cell cost

--- om_ws/src/om17/src/world.cpp
+++ om_ws/src/om17/src/world.cpp
@@ -23,6 +23,7 @@
 #include "std_msgs/MultiArrayDimension.h"    // For formatting array-types
 #include "std_msgs/Int32MultiArray.h"        // For ROS vector-type messages
 
+#include "om17/CellCost.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // CONSTANTS
@@ -44,15 +45,14 @@ const float TILE_SIZE = 7.38f / (float)GRID_WIDTH;
 ////////////////////////////////////////////////////////////////////////////////
 
 char** grid = nullptr;
-float** cost_map = nullptr;
 
-ros::Publisher  world_pub;
 ros::Subscriber phobos_pose_sub;
 ros::Subscriber deimos_pose_sub;
+ros::Subscriber world_cost_sub;
 
 // phobos grid position
-unsigned phobos_x = 5;
-unsigned phobos_y = 9;
+unsigned phobos_x = 0;
+unsigned phobos_y = 0;
 
 // deimmos grid position
 unsigned deimos_x = 0;
@@ -66,8 +66,9 @@ void init(void);
 void tick(void);
 void cleanup(void);
 
-void phobos_pose_callback(const geometry_msgs::Pose2D& pose);
-void deimos_pose_callback(const geometry_msgs::Pose2D& pose);
+void phobosPoseCallback(const geometry_msgs::Pose2D& pose);
+void deimosPoseCallback(const geometry_msgs::Pose2D& pose);
+void worldCostCallback(const om17::CellCost& cell_cost);
 
 ////////////////////////////////////////////////////////////////////////////////
 // ENTRY POINT
@@ -82,8 +83,9 @@ int main(int argc, char** argv)
 
     init();
 
-    phobos_pose_sub = node_handle.subscribe("/phobos/pose", 1, phobos_pose_callback);
-    deimos_pose_sub = node_handle.subscribe("/deimos/pose", 1, deimos_pose_callback);
+    phobos_pose_sub = node_handle.subscribe("/phobos/pose", 1, phobosPoseCallback);
+    deimos_pose_sub = node_handle.subscribe("/deimos/pose", 1, deimosPoseCallback);
+    world_cost_sub = node_handle.subscribe("/world_cost", 1, worldCostCallback)
 
     while (ros::ok())
     {
@@ -106,12 +108,10 @@ int main(int argc, char** argv)
 void init(void)
 {
     grid = new char*[GRID_HEIGHT];
-    cost_map = new float*[GRID_HEIGHT];
 
     for (unsigned i = 0; i < GRID_HEIGHT; ++i)
     {
         grid[i] = new char[GRID_WIDTH];
-        cost_map[i] = new float[GRID_WIDTH];
     }
 
     // initialize grid values here
@@ -120,7 +120,6 @@ void init(void)
         for (unsigned x = 0; x < GRID_WIDTH; ++x)
         {
             grid[y][x] = '.';
-            cost_map[y][x] = 0.0f;
         }
     }
 }
@@ -154,20 +153,23 @@ void cleanup(void)
     for (unsigned i = 0; i < GRID_HEIGHT; ++i)
     {
         delete [] grid[i];
-        delete [] cost_map[i];
     }
     delete [] grid;
-    delete [] cost_map;
 }
 
-void phobos_pose_callback(const geometry_msgs::Pose2D& pose)
+void phobosPoseCallback(const geometry_msgs::Pose2D& pose)
 {
     phobos_x = (unsigned)(pose.x / TILE_SIZE);
     phobos_y = (unsigned)(pose.y / TILE_SIZE);
 }
 
-void deimos_pose_callback(const geometry_msgs::Pose2D& pose)
+void deimosPoseCallback(const geometry_msgs::Pose2D& pose)
 {
     deimos_x = (unsigned)(pose.x / TILE_SIZE);
     deimos_y = (unsigned)(pose.y / TILE_SIZE);
+}
+
+void worldCostCallback(const om17::CellCost& cell_cost)
+{
+
 }
