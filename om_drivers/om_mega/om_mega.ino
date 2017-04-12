cfed268b4616a0d0a176d56fa7e11c27ffbe6e89
harris_n <newstederharris@gmail.com>
2017-04-12 16:29:09 -0400

    beginning to add code for TOF sensors to the drivers portion

--- om_drivers/om_mega/om_mega.ino
+++ om_drivers/om_mega/om_mega.ino
@@ -9,18 +9,30 @@
 // INCLUDES
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "ir_sense.h"
-
 #include <ros.h>
+
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int8.h>
+
+#include <Wire.h>
+#include <VL53L0X.h>
 #include <Servo.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // CONSTANTS
 ////////////////////////////////////////////////////////////////////////////////
 
-// the pins that control the automotive relays
+// pwm pins which drive the motor controllers
+const int MC1 = 3;
+const int MC2 = 2;
+
+// limit switch input pins
+const int LS_EXCV_EXTENDED = 26;
+const int LS_EXCV_RETRACTED = 27;
+const int LS_DEPO_UP = 28;
+const int LS_DEPO_DOWN = 29;
+
+// the pins that control the states of the automotive relays
 const int RELAY_1 = 8;
 const int RELAY_2 = 9;
 const int RELAY_3 = 10;
@@ -43,8 +55,10 @@ const int RELAY_STATES[3][3] = {
 
 ros::NodeHandle node_handle;
 
-Servo mc1;
-Servo mc2;
+Servo mc1, mc2;
+Servo tf1_servo, tf2_servo;
+
+VL53L0X tf1, tf2;
 
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION DECLARATIONS
@@ -54,25 +68,40 @@ void mc1Callback(const std_msgs::Int16& msg);
 void mc2Callback(const std_msgs::Int16& msg);
 void setControlState(int control_state);
 
-ros::Subscriber<std_msgs::Int16> mc1_sub("lcv", &mc1Callback);
-ros::Subscriber<std_msgs::Int16> mc2_sub("rcv", &mc2Callback);
+ros::Subscriber<std_msgs::Int16> mc1_sub("mc1", &mc1Callback);
+ros::Subscriber<std_msgs::Int16> mc2_sub("mc2", &mc2Callback);
 
 ////////////////////////////////////////////////////////////////////////////////
 // ARDUINO FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
 
 void setup()
-{
+{ 
   node_handle.initNode();
-  node_handle.subscribe(lcv_sub);
-  node_handle.subscribe(rcv_sub);
+
+  Wire.begin();
+  
+  node_handle.subscribe(mc1_sub);
+  node_handle.subscribe(mc2_sub);
 
   pinMode(RELAY_1, OUTPUT);
   pinMode(RELAY_2, OUTPUT);
   pinMode(RELAY_3, OUTPUT);
+
+  pinMode(LS_EXCV_EXTENDED, INPUT);
+  pinMode(LS_EXCV_RETRACTED, INPUT);
+  pinMode(LS_DEPO_UP, INPUT);
+  pinMode(LS_DEPO_DOWN, INPUT);
   
-  mc1.attach(3);
-  mc2.attach(2);
+  mc1.attach(MC1);
+  mc2.attach(MC2);
+
+  // initialize the TOF sensors
+  tf1.init();
+  tf1.setMeasurementTimingBudget(200000);
+  // TODO: change address of tf2 on the bus to 42
+  tf2.init();
+  tf2.setMeasurementTimingBudget(200000);
 }
 
 void loop()
@@ -87,11 +116,13 @@ void loop()
 
 void mc1Callback(const std_msgs::Int16& msg)
 {
+  // convert message value of [-100, 100] to [1000, 2000]
   mc1.writeMicroseconds(((int)msg.data * 5) + 1500);
 }
 
 void mc2Callback(const std_msgs::Int16& msg)
 {
+  // convert message value of [-100, 100] to [1000, 2000]
   mc2.writeMicroseconds(((int)msg.data * 5) + 1500);
 }
 
