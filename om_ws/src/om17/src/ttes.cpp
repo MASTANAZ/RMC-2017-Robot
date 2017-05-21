5381f99da8c7736d5656de9999e824f0748b571c
blake <blake.nazario@yahoo.com>
2017-05-21 18:15:02 -0400

    Added PID line follower prototype to TTES.cpp IT WILL NEED WORK

--- om_ws/src/om17/src/ttes.cpp
+++ om_ws/src/om17/src/ttes.cpp
@@ -22,12 +22,43 @@ void ttes::init(Robot* robot)
     timer = 0.0f;
 }
 
+// We don't need a timeout because AutoControls takes care of that
+
+#warning K-Proportion and K-Derivative need to be determined experimentally
+float k_p = 0.0;
+float k_d = 0.0;
+
+float last_x_error = 0.0;
+float last_y_error = 0.0;
+float last_theta_error = 0.0;
+
 void ttes::tick(float dt, Robot* robot)
 {   
-    // TODO: line following
-
-    robot->mc1 = 100;
-    robot->mc2 = 100;
+    // This code may be very very shit, it hasnt been tested. The robot will probalby be extremely jerky.
+    
+#warning The value for x,y,theta-errors will need to be determined experimentally
+    // Error for (x,y,theta)
+    //TODO: This error will need to relate to the error from the position of the planned path
+    //      relative to the rover.
+    float x_error     = 0.75 * robot->x;
+    float y_error     = 0.78 * robot->y;
+    float theta_error = 0.65 * robot->theta; // This will require a kalman filter to be truly useful
+    
+    float error_avg = (x_error +y_error + theta_error) / 3;
+    
+    int motor_speed = k_p * error_avg + k_d * (error_avg - ((last_x_error + last_y_error + last_theta_error)/3));
+    
+    last_x_error = x_error;
+    last_y_error = y_error;
+    last_theta_error = theta_error;
+    
+    robot->mc2 = 75.0 + motor_speed;
+    robot->mc1 = 75.0 - motor_speed;
+    
+    
+    
+//    robot->mc1 = 100;
+//    robot->mc2 = 100;
 
     
     if (robot->x >= 4.4)
